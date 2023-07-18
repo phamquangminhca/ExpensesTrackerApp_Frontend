372b4ee0f6473f6b18f7affa2e992c8034f6b9a7
Quang Minh Pham <phamquangminhca@Quangs-MacBook-Pro.local>
2023-07-18 00:11:04 -0500

    Added some components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Login from './components/Forms/Login';
+import HomePage from './components/HomePage/HomePage';
+import Register from './components/Forms/Register';
+import Navbar from './components/Navbar/Navbar';
+import AddTransaction from './components/Forms/AddTransaction';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/add-transaction' element={<AddTransaction/>}/>
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
